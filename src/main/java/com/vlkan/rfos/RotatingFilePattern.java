17debfd84c98218c0697f98ea236b206b9e47718
Volkan Yazici <volkan.yazici@gmail.com>
2020-01-10 21:23:45 +0100

    Update copyright year.

--- src/main/java/com/vlkan/rfos/RotatingFilePattern.java
+++ src/main/java/com/vlkan/rfos/RotatingFilePattern.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Volkan Yazıcı
+ * Copyright 2018-2020 Volkan Yazıcı
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
